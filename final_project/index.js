2bdf2f1483c0195b6e294aee469757120f5d5671
Chris Lockwood <chrislockwoodmarketing@gmail.com>
2025-03-01 13:21:32 -0500

    index

--- final_project/index.js
+++ final_project/index.js
@@ -10,9 +10,26 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    // Get the token from headers
+    const token = req.headers['authorization'];
+
+    if (!token) {
+        return res.status(403).json({ message: "Access Denied: No Token Provided" });
+    }
+
+    // Verify token
+    jwt.verify(token.split(" ")[1], "your_secret_key", (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: "Invalid Token" });
+        }
+
+        // Attach user information to request
+        req.user = decoded;
+        next();
+    });
 });
+
  
 const PORT =5000;
 
