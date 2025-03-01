c48673f5b8efe2691a556b45291bc77fefdad1d1
Chris Lockwood <chrislockwoodmarketing@gmail.com>
2025-03-01 14:10:25 -0500

    task 3-4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,15 +27,26 @@ public_users.get('/isbn/:isbn', function (req, res) {
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author; // Get author
+    const booksByAuthor = Object.values(books).filter(book => book.author === author);  
+    
+    if (booksByAuthor.length > 0) {
+        return res.status(200).json(booksByAuthor); // Return book details if found
+    } else {
+        return res.status(404).json({ message: "No books found by this author" }); // Return error if not found
+    }
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title // Get title
+    const booksByTitle = Object.values(books).filter(book => book.title === title);  
+    
+    if (booksByTitle.length > 0) {
+        return res.status(200).json(booksByTitle);  
+    } else {
+        return res.status(404).json({ message: "No books found with this title" });      }
 });
 
 //  Get book review
